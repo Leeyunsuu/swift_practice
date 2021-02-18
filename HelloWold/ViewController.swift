3c1d93eb7a208f2fe59655e67f5c8b59d091c216
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-02-18 17:19:51 +0900

    Swift_practice start

--- HelloWold/ViewController.swift
+++ HelloWold/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var Hello: UILabel! //출력 전용 변수
+  @IBOutlet var Namebox: UITextField! //Name 입력 변수
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+
+  @IBAction func DoneBtn(_ sender: UIButton) {
+    Hello.text = "Hello, " + Namebox.text! + "!!"
+  }
+  //Done버튼을 클릭했을 떄
+
 
 
 }
